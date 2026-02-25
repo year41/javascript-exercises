0ced5903cd67f5922439bba695c48522b37a0c6e
dado <162951673+year41@users.noreply.github.com>
2026-02-25 13:13:36 +1100

    Solve exercise 08_removeFromArray
    Solved the exercise a bit different from solutions provided, but i
    feel satisfied with my solution.

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,27 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...remove) {
+    for (const i of remove) {
+        arr = arr.filter(item => item !== i);
+    };
+    return arr;
 };
+
+//Solutions provided
+const removeFromArray1 = function(array, ...args) {
+  return array.filter(val => !args.includes(val));
+};
+
+//Second solution provided
+const removeFromArray2 = function (array, ...args) {
+    const newArray = [];
+    array.forEach((item) => {
+        if (!args.includes(item)) {
+        newArray.push(item);
+        }
+    });
+    return newArray;
+};
+
+console.log(removeFromArray([1, 2, 3, 4], 3))
 
 // Do not edit below this line
 module.exports = removeFromArray;
