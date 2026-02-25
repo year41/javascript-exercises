f5915451c37830af232d06fc0a95477c14e68c8a
dado <162951673+year41@users.noreply.github.com>
2026-02-25 17:13:33 +1100

    Solve exercise 11_temConversion.

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+function rounDec(num) {
+  return Math.round(num *10) / 10;
+}
+
+const convertToCelsius = function(farTemp) {
+  return rounDec((farTemp -32) * (5 / 9));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cenTemp) {
+  return rounDec((cenTemp * (9 / 5)) + 32);
 };
 
 // Do not edit below this line
