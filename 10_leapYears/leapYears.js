3c862a66d348400679c063e82188d863374c2442
dado <162951673+year41@users.noreply.github.com>
2026-02-25 16:52:44 +1100

    Solve exercise 10_leapYears

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,20 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if (year % 400 == 0) {
+        return true;
+    } else if ((year % 4 == 0) && !(year % 100 ==0)) {
+        return true;
+    } else{
+        return false;
+    };
+};
 
+//My interpretation of suggested solution
+const leapYears1 = function(year) {
+    if (year % 4 == 0 && (!(year % 100 ==0) || year % 400 == 0)) {
+        return true;
+    } else{
+        return false;
+    };
 };
 
 // Do not edit below this line
