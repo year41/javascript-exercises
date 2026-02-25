5d6649bca58f16d9519fc5d2042b9168c9dfd1d3
dado <162951673+year41@users.noreply.github.com>
2026-02-25 12:30:24 +1100

    Solve exercise 07_reverseString
    Solved the exercise using s for loop but on the provided solution
    they provided a single line solution using just Array methods which
    is more simple and clear. From the next one i'll try to think about
    using the array methods to solve the exercises.

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let reverseStr = '';
+    for (let i = (str.length - 1); i >= 0; i--) {
+        reverseStr += str[i];
+    }
+    return reverseStr
+};
 
+const reverseString2 = function (str) {
+  return str.split("").reverse().join("");
 };
 
 // Do not edit below this line
