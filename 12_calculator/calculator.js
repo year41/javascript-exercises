c6f4e3111153e690c7c7f4eaf1a5e1e0b118eba5
dado <162951673+year41@users.noreply.github.com>
2026-03-06 10:09:57 +1100

    Solve exercise 12_calculator

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, curr) => total + curr, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, curr) => total * curr, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let fac = 1
+  for (let i = a; i > 0 ; i--) {
+    fac *= i;
+  };
+  return fac;
 };
 
 // Do not edit below this line
