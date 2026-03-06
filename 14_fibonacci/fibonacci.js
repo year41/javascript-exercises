9f0dec59177e1c82fa927f8ebde1bbadbd575c5c
dado <162951673+year41@users.noreply.github.com>
2026-03-06 12:39:40 +1100

    Solve exercise 14_fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+    if (num < 0) return "OOPS";
+    if (num == 0) return 0;
 
+    let first = 1;
+    let next = 0;
+
+    for (let i = 2; i <= num; i++) {
+        let current = first + next;
+        next = first;
+        first = current;
+    };
+    return first;
+
+    //Solution suggestion.
+    // const fib = [0, 1];
+    // for (let i = 2; i <= num; i++) {
+    //     fib[i] = fib[i - 1] + fib[i - 2];
+    // }
+    // return fib[num];
 };
 
 // Do not edit below this line
