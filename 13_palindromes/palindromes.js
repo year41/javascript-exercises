ec4ac33b17bb0284b629b0c1a9a243e2d225dd46
dado <162951673+year41@users.noreply.github.com>
2026-03-06 11:12:25 +1100

    Solve exercise 13_palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,23 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let cleanStr = str
+        .toLowerCase()
+        .replace(/[^a-zA-Z0-9]/g, '');
 
+    //Suggested solution
+    const reverStr = cleanStr
+        .split('')
+        .reverse()
+        .join('');
+
+    return cleanStr === reverStr;
+
+    //My solution \/
+    // for (let i = 0; i < cleanStr.length; i++) {
+    //     if (cleanStr[i] != cleanStr[cleanStr.length - 1 - i]) {
+    //         return false;
+    //     };
+    // };
+    // return true;
 };
 
 // Do not edit below this line
